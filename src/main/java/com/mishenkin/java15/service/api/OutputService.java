5b26355446971054a32410e9a3cc2682b83acdf3
psypr0ff <psypr0ff@gmail.com>
2018-10-31 00:11:59 +0400

    Add service api

--- src/main/java/com/mishenkin/java15/service/api/OutputService.java
+++ src/main/java/com/mishenkin/java15/service/api/OutputService.java
@@ -1,7 +1,13 @@
 package com.mishenkin.java15.service.api;
 
 /**
+ * Сервис для создания HTML файла для резюме
  * Created by Александр on 30.10.2018.
  */
 public interface OutputService {
+    /**
+     * Метод для содздания html-файла
+     * @param Path - путь для сохранения html-файла
+     */
+    void createHtmlFile(String Path);
 }
