7f3f58c692ea62cf5b4c8be4bba362c8d84f2f0a
psypr0ff <psypr0ff@gmail.com>
2018-10-30 23:13:09 +0400

    Add common constants

--- src/main/java/com/mishenkin/java15/common/constants/PersonPropertyKeys.java
+++ src/main/java/com/mishenkin/java15/common/constants/PersonPropertyKeys.java
@@ -1,7 +1,67 @@
 package com.mishenkin.java15.common.constants;
 
 /**
+ * Хранилище ключей из .properties файла
  * Created by Александр on 30.10.2018.
  */
 public interface PersonPropertyKeys {
+    /**
+     * Свойство - Фамилия Имя Отчество
+     */
+    String FIO = "FIO";
+
+    /**
+     * Свойство - дата рождения
+     */
+    String DOB = "DOB";
+
+    /**
+     * Свойство - телефон
+     */
+    String PHONE = "phone";
+
+    /**
+     * Свойство - электронная почта
+     */
+    String EMAIL = "email";
+
+    /**
+     * Свойство - скайп
+     */
+    String SKYPE = "skype";
+
+    /**
+     * Свойство - ссылка на аватарку
+     */
+    String AVATAR = "avatar";
+
+    /**
+     * Свойство - цель
+     */
+    String TARGET = "target";
+
+    /**
+     * Свойство - опыт работы
+     */
+    String EXPERIENCES = "experiences";
+
+    /**
+     * Свойство - образование
+     */
+    String EDUCATIONS = "educations";
+
+    /**
+     * Свойство - дополнительное образование
+     */
+    String ADDITIONAL_EDUCATIONS = "additional_educations";
+
+    /**
+     * Свойство - скилы
+     */
+    String SKILLS = "skills";
+
+    /**
+     * Свойство - примеры кода
+     */
+    String EXAMPLES_CODE = "examples_code";
 }
