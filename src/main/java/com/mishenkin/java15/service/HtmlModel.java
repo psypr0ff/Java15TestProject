e0bdfb97098e84135f4e22efc2eb55254257bbc5
psypr0ff <psypr0ff@gmail.com>
2018-10-31 00:35:35 +0400

    Add service implementation

---
+++ src/main/java/com/mishenkin/java15/service/HtmlModel.java
@@ -0,0 +1,7 @@
+package com.mishenkin.java15.service;
+
+/**
+ * Created by Александр on 31.10.2018.
+ */
+public class HtmlModel {
+}
--- src/main/java/com/mishenkin/java15/service/impl/OutputServiceImpl.java
+++ src/main/java/com/mishenkin/java15/service/impl/OutputServiceImpl.java
@@ -1,7 +1,31 @@
 package com.mishenkin.java15.service.impl;
 
+import com.mishenkin.java15.dao.repository.PersonRepository;
+import com.mishenkin.java15.dao.repository.impl.PersonRepositoryFromPropertyFileImpl;
+import com.mishenkin.java15.domain.entity.PersonalData;
+import com.mishenkin.java15.service.api.OutputService;
+
 /**
  * Created by Александр on 30.10.2018.
  */
-public class OutputServiceImpl {
+public class OutputServiceImpl implements OutputService{
+    private PersonalData personalData;
+
+    /**
+     * Метод для подключения .properties файла
+     * @param propertyFilePath - путь к .properties файла
+     */
+    public OutputServiceImpl(String propertyFilePath){
+        PersonRepository personRepository = new PersonRepositoryFromPropertyFileImpl(
+                getClass().getClassLoader().getResourceAsStream(propertyFilePath)
+        );
+        this.personalData = personRepository.getPersonalData();
+    }
+
+    @Override
+    public void createHtmlFile(String outputHttpFilePath){
+        if (this.personalData != null){
+
+        }
+    }
 }
